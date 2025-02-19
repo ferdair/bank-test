43c3ce74c08babb40c406483b442e050fedab577
Daír Sánchez <54245748+ferdair@users.noreply.github.com>
2025-02-18 20:28:23 -0500

    Se configura el service

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable, signal } from '@angular/core';
+import { Product, ProductResponse } from '@shared/models';
+import { environment } from '@config/api.config';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  private apiUrl = environment.apiUrl;
+  private endpoints = environment.endpoints.products;
+  private products = signal<Product[]>([]);
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  getProducts() {
+    const url = `${this.apiUrl}${this.endpoints.base}`;
+    return this.http.get<ProductResponse>(url).subscribe({
+      next: (response) => {
+        this.products.set(response.data);
+      },
+      error: (error) => {
+        console.error('Error fetching products:', error);
+      },
+    });
+  }
 }
